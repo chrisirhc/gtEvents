113812b93e467a730c9c4f54854cdce3c389b2a2
Chris Chua <chris.sirhc@gmail.com>
2011-02-02 23:41:05 -0500

    Draw actual data from chrisirhc.no.de . :)

--- gtevents.js
+++ gtevents.js
@@ -55,5 +55,34 @@ $(document).ready(function(){
 		$extendedPanel.slideToggle('fast');
 	},false)
 	
-	
+	/* Start the grabbing of the data from the server */
+	/* Not sure if this cross-domain request works on the phones */
+
+	$.getJSON("http://chrisirhc.no.de/?callback=?", function (data, status) {
+    var i, eObj;
+    for (i = 0; i < data.length && (eObj = data[i]); i++) {
+        $("#eventlist").append(
+           	'<li><a href="#bar">'
+           	+ '<div class="event-list-time"><span class="event-list-time-span">'
+           	+ eObj["start_time"]
+           	+ '</span> - <span class="event-list-time-span">'
+           	+ eObj["end_time"]
+           	+ '</span></div>'
+           	+ '<div class="event-list-title-container">'
+	  		+ '<div class="event-list-thumbnail"></div>'
+	  		+ '<div class="event-list-title-details-container">'
+	  		+ '<div class="event-list-title">' + eObj["name"] + '</div>'
+		  	+ '<div class="event-list-organizer">by ' + eObj["organizer"] + '</div>'
+		  	+ '<div class="event-list-location">at ' + eObj["location"] + '</div>'
+		  	+ '</div>'
+    		+ '</div>'
+    		+ '<div class="clear"></div>'
+      		+ '<div class="event-list-tab">'
+      		+ '<span class="event-list-tab-friend">35 friends</span>'
+                + '<span class="event-list-tab-attendees">146 attendees</span>'
+      		+ '</div>'
+            + '</a></li>');
+    }
+    $("#eventlist").listview("refresh");
+  });
 });
