d31b5d33abdc56a6b4bf337c3bdc30c82f567bc9
Felix Lau <felixlaumon@gmail.com>
2011-02-09 00:52:04 -0500

    added offline storage (lawnchair); implemented description, wall, attendees, and thumbnails

--- gtevents.js
+++ gtevents.js
@@ -1,16 +1,16 @@
-/* global variable */
-var detailEventId;
-
 /* shorthand method for debugging */
 function log(x){
 	console.log(x);
 }
 /* shorthand method for debugging */
 
+var offlineStore;
+var detailEventId;
 $(document).ready(function(){
 	
 	/* http://forum.jquery.com/topic/jquery-mobile-equivalent-of-document-ready */
 	/* $(document).ready for each jquery mobile */
+	/* TODO: Fire ready functions when the page is refreshed */
 	if (typeof GTEVENTS == 'undefined') GTEVENTS = {};
 	if (typeof GTEVENTS.Pages == 'undefined') GTEVENTS.Pages = {};
 	jQuery("div[data-role*='page']").live('pageshow', function(event, ui) {
@@ -20,6 +20,15 @@ $(document).ready(function(){
                 GTEVENTS.Pages[thisId].call(this);
         }
 	});
+	
+	/* offline storage init */
+	offlineStore = new Lawnchair('gtevents');
+	
+	try {
+		offlineStore.get('detailEventId', function(r){
+			detailEventId = r.id;
+		});
+	} catch(err) {}
 	
 	$('.event-list-tab-friend').bind('click',function(event){
 		/* do not follow link when click on tab in list */
@@ -34,11 +43,13 @@ $(document).ready(function(){
 	
 	/* Start the grabbing of the data from the server */
 
-	$.getJSON("http://chrisirhc.no.de/?callback=?", function (data, status) {
+	$.getJSON("http://gtevents.localhost:3000/list?callback=?", function (data, status) {
     var i, eObj;
     for (i = 0; i < data.length && (eObj = data[i]); i++) {
+    	log(eObj);
+    	offlineStore.save({key:eObj["eid"], obj: eObj});
         $("#eventlist").append(
-           	'<li id="'+ eObj["id"] +'"><a href="#detail">'
+           	'<li id="'+ eObj["eid"] +'"><a href="#detail">'
            	+ '<div class="event-list-time">'
            	+ parseTime(eObj["start_time"])[0]
            	+'<span class="event-list-time-span">'
@@ -49,10 +60,12 @@ $(document).ready(function(){
            	+ parseTime(eObj["end_time"])[1]
            	+ '</span></div>'
            	+ '<div class="event-list-title-container">'
-	  		+ '<div class="event-list-thumbnail"></div>'
+	  		+ '<div class="event-list-thumbnail" style="background-image: url('
+	  		+ eObj['pic']
+	  		+')"></div>'
 	  		+ '<div class="event-list-title-details-container">'
 	  		+ '<div class="event-list-title">' + eObj["name"] + '</div>'
-		  	+ '<div class="event-list-organizer">by ' + eObj["organizer"] + '</div>'
+		  	+ '<div class="event-list-organizer">by ' + eObj["host"] + '</div>'
 		  	+ '<div class="event-list-location">at ' + eObj["location"] + '</div>'
 		  	+ '</div>'
     		+ '</div>'
@@ -65,9 +78,10 @@ $(document).ready(function(){
     }
     $("#eventlist").listview("refresh");
     
-    /* store event id into global */
+    /* store event id into database and change from list to detail view */
     $('#eventlist > li').bind('click', function(event){
-		detailEventId = ($(this).attr('id'));
+		var detailEventId = ($(this).attr('id'));
+		offlineStore.save({key:'detailEventId', id: detailEventId});
 		
 		/* manually handle link forwarding */
 		event.stopPropagation();
@@ -76,7 +90,20 @@ $(document).ready(function(){
 		$.mobile.changePage('#detail', 'slide', false, true);
 	});
 	
+	
+	/* detail view page ready function */
 	GTEVENTS.Pages.detail = function(){
+		var eventObj;
+		offlineStore.get('detailEventId', function(r){
+			detailEventId = r.id;
+		});
+		log(detailEventId);
+		
+		/* not used for now */
+		offlineStore.get(detailEventId, function(r){
+			eventObj = r.obj;
+		});
+		log(eventObj);
 		$.getJSON("http://chrisirhc.no.de/" + detailEventId + "?callback=?" , function(data,status){
 			log(data);
 			$('.detail-view-title').html(data.name);
@@ -85,12 +112,79 @@ $(document).ready(function(){
 			$('#detail-view-end-li span').html( parseTime(data.end_time)[0] + " " + parseTime(data.end_time)[1]);
 		});
 		
-		/* !global unbind for all div. consider another way */
-		$('div').unbind('pageshow');
 	}
 	
-	GTEVENTS.Pages.friend = function(){
+	/* attendees view ready function */
+	GTEVENTS.Pages.attendees = function(){
+		$.mobile.pageLoading();
+		$("#attendees-view-list").html('');
+		$.getJSON("http://localhost:3000/rsvp_attend/" + detailEventId + "?callback=?" , function(data,status){
+			for (i=0; i<data.length; i++){
+				$("#attendees-view-list").append(
+					'<li><div class="attendees-view-thumbnail" style="background-image: url('
+					+ 'https://graph.facebook.com/' + data[i].id + '/picture'
+					+ ');">'
+					+ '</div><h3 class="attendees-view-name">'
+					+ data[i].name
+					+ '</h3></div>'
+				);
+			}
+			$("#attendees-view-list").listview("refresh");
+			$.mobile.pageLoading(true);
+		});
+	}
+	
+	GTEVENTS.Pages.wall = function(){
+		$.mobile.pageLoading();
+		$("#wall-view-list").html('');
+		$.getJSON("http://localhost:3000/eventwall/" + detailEventId + "?callback=?" , function(data,status){
+			for (i=0; i<data.length; i++){
+				$("#wall-view-list").append(
+					'<li><div class="wall-view-thumbnail" style="background-image: url('
+					+ 'https://graph.facebook.com/' + data[i].id + '/picture'
+					+')"></div><div class="wall-view-content-container"><p class="wall-view-name">'
+					+ data[i].name
+					+ '</p><p class="wall-view-content">'
+					+ data[i].message
+					+ '</p><p class="wall-view-post-time">'
+					+ parseTime(data[i].update_time)[0]
+					+ ' ' + parseTime(data[i].update_time)[1]
+					+ '</p></div></li>'
+				);
+			}
+			$("#wall-view-list").listview("refresh");
+			$.mobile.pageLoading(true);
+		});
+	}
+	
+	GTEVENTS.Pages.description = function(){
+		$.mobile.pageLoading();
+		offlineStore.get(detailEventId, function(r){
+			var eventObj = r.obj;
+			log(eventObj.description);
+			$('#description-view-content').html(eventObj.description.replace(/\n/g,'<br />'));
+		});
 		
+		
+		$.getJSON("http://localhost:3000/eventwall/" + detailEventId + "?callback=?" , function(data,status){
+			
+			for (i=0; i<data.length; i++){
+				$("#wall-view-list").append(
+					'<li><div class="wall-view-thumbnail" style="background-image: url('
+					+ 'https://graph.facebook.com/' + data[i].id + '/picture'
+					+')"></div><div class="wall-view-content-container"><p class="wall-view-name">'
+					+ data[i].name
+					+ '</p><p class="wall-view-content">'
+					+ data[i].message
+					+ '</p><p class="wall-view-post-time">'
+					+ parseTime(data[i].update_time)[0]
+					+ ' ' + parseTime(data[i].update_time)[1]
+					+ '</p></div></li>'
+				);
+			}
+			$("#wall-view-list").listview("refresh");
+			$.mobile.pageLoading(true);
+		});
 	}
     
   });
@@ -112,7 +206,7 @@ function parseTime(time){
 	else if (hour>12) hour-=12;
 	
 	var minute = t.getMinutes();
-	if (minute == 0) minute = "00";
+	if (minute < 10) minute = '0' + minute;
 	
 	var output = new Array;
 	output[0] = month + " " + day + " "
